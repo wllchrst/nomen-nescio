b16e381f8562b3104ddb136a2dac3bca7e42e24d
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-14 19:00:35 +0700

    feat: signature model change logic

--- ai/service/signature_model.py
+++ ai/service/signature_model.py
@@ -5,17 +5,16 @@ from matplotlib import pyplot as plt
 
 class SignatureModel :
   def __init__(self) -> None:
-    # self.model = tf.keras.models.load_model('./model/best_model.keras')
+    self.model = tf.keras.models.load_model('./model/best_model.keras', safe_mode=False)
     pass
 
   def preprocess_image(self):
-    result = self.image_processor.preprocess_image(self.verification_data.target_signature_image)
-    plt.imshow(result)
-    plt.show()
-    pass
+    self.user_preprocessed= self.image_processor.preprocess_image(self.verification_data.user_signature_image)
+    self.target_preprocessed= self.image_processor.preprocess_image(self.verification_data.target_signature_image)
   
   def predict(self, verification_data: VerificationData) -> bool:
     self.verification_data = verification_data
     self.image_processor = ImageProcessor(self.verification_data)
     self.preprocess_image()
+    # Image processed, just predict.
     pass
