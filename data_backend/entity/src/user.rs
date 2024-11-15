bafa40003771d40daea3cc2a867d6b942956f607
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-15 18:13:17 +0700

    feat: user relation with group members

--- data_backend/entity/src/user.rs
+++ data_backend/entity/src/user.rs
@@ -15,6 +15,15 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::group_member::Entity")]
+    GroupMember,
+}
+
+impl Related<super::group_member::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::GroupMember.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
