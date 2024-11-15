be959bbe3a92084a68abb805e2c5def1e754602f
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-15 18:12:32 +0700

    feat: Add method for predicting

--- ai/service/signature_model.py
+++ ai/service/signature_model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from service.interfaces import VerificationData
 from service.image_processor import ImageProcessor
 from matplotlib import pyplot as plt
+import numpy as np
 
 class SignatureModel :
   def __init__(self) -> None:
@@ -11,10 +12,16 @@ class SignatureModel :
   def preprocess_image(self):
     self.user_preprocessed= self.image_processor.preprocess_image(self.verification_data.user_signature_image)
     self.target_preprocessed= self.image_processor.preprocess_image(self.verification_data.target_signature_image)
+
+  def get_prediction(self):
+    data = {"image1": self.user_preprocessed, "image2": self.target_preprocessed}
+    prediction = self.model.predict(data)
+    print(f"Prediction: {prediction}")
   
   def predict(self, verification_data: VerificationData) -> bool:
     self.verification_data = verification_data
     self.image_processor = ImageProcessor(self.verification_data)
     self.preprocess_image()
     # Image processed, just predict.
+    self.get_prediction()
     pass
