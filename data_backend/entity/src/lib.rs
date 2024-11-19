0db56c52ca7f85619443f2164c3ce1bd1f2011a7
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-19 14:48:37 +0700

    feat: add dependency to entity crate

--- data_backend/entity/src/lib.rs
+++ data_backend/entity/src/lib.rs
@@ -1,7 +1,10 @@
+#[macro_use]
+extern crate rocket;
+
+pub mod email;
+pub mod email_file;
 pub mod group;
 pub mod group_member;
 pub mod prelude;
-pub mod email;
-pub mod email_file;
 pub mod receiver;
 pub mod user;
