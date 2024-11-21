1b226aad264f5ef197545be015acedd0d17c9ef2
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-21 14:41:03 +0700

    feat: models needed for api

--- data_backend/api/src/models/request/create_user.rs
+++ data_backend/api/src/models/request/create_user.rs
@@ -1,6 +1,9 @@
-#[derive(FromForm)]
+use serde::Deserialize;
+
+#[derive(FromForm, Deserialize)]
 pub struct CreateUserData {
     pub name: String,
     pub email: String,
     pub password: String,
+    pub signature_file_path: String,
 }
