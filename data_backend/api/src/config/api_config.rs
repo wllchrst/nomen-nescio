d7174482672257d6fb7a2a6d0a18b83e25eef637
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-23 17:51:44 +0700

    feat: allow all origin

--- data_backend/api/src/config/api_config.rs
+++ data_backend/api/src/config/api_config.rs
@@ -1,9 +1,10 @@
 use std::env;
 
 use dotenv::dotenv;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 use sea_orm::{Database, DatabaseConnection};
 
-use crate::routes::{file_routes::upload_file, test, user_routes::create_user};
+use crate::routes::{file_routes::upload_file, test, user_routes::{create_user, handle_login}};
 
 #[tokio::main]
 pub async fn start() -> Result<(), rocket::Error> {
@@ -15,9 +16,15 @@ pub async fn start() -> Result<(), rocket::Error> {
         .await
         .expect("Failed to connect to database");
 
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .to_cors()
+        .expect("Failed to create cors config");
+
     rocket::build()
+        .attach(cors)
         .manage(database)
-        .mount("/", routes![test, create_user, upload_file])
+        .mount("/", routes![test, create_user, upload_file, handle_login])
         .launch()
         .await
         .map(|_| ())
