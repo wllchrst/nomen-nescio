4ca08ae4bc6e0738e55fdc6f88cb5db81b13742c
Marvel Collin <marvelcollin7@gmail.com>
2024-11-24 20:32:45 +0700

    fix: uncomment authentication

--- src/nomennescio_frontend/src/pages/authentication/Register.tsx
+++ src/nomennescio_frontend/src/pages/authentication/Register.tsx
@@ -7,42 +7,42 @@ import DrawableCanvas from "../../components/elements/canvas/drawable-canvas";
 import TypingEffect from "react-typing-effect";
 import useWords from "../../hooks/use-authentication-words";
 import ParticleBackground from "../../components/elements/canvas/particle-background";
-// import { SubmitHandler, useForm } from "react-hook-form";
-// import { IRegister } from "../../interfaces/register-interface";
-// import { GeneralService } from "../../service/general-service";
-// import { UserService } from "../../service/user-service";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { IRegister } from "../../interfaces/register-interface";
+import { GeneralService } from "../../service/general-service";
+import { UserService } from "../../service/user-service";
 
 const Register: React.FC = () => {
   const words = useWords();
   const navigate = useNavigate();
-  // const { register, handleSubmit } = useForm<IRegister>();
+  const { register, handleSubmit } = useForm<IRegister>();
   const [file, setFile] = useState<File | null>(null);
-  // const generalService = new GeneralService();
+  const generalService = new GeneralService();
   // const userService = new UserService();
 
   function changeFile(inputFile: File | null) {
     setFile(inputFile);
   }
 
-  // const onSubmit: SubmitHandler<IRegister> = async (data) => {
-  //   if (file == null) {
-  //     alert("Please make sure that you have done your signature.");
-  //     return;
-  //   }
+  const onSubmit: SubmitHandler<IRegister> = async (data) => {
+    if (file == null) {
+      alert("Please make sure that you have done your signature.");
+      return;
+    }
 
-  //   const path = await generalService.uploadFile(file);
-  //   console.log(`PATH: ${path}`);
+    const path = await generalService.uploadFile(file);
+    console.log(`PATH: ${path}`);
 
-  //   if (path == "") {
-  //     console.log("something went wrong");
-  //     return;
-  //   }
+    if (path == "") {
+      console.log("something went wrong");
+      return;
+    }
 
-  //   data.signature_file_path = path;
-  //   const result = await userService.registerUser(data);
+    data.signature_file_path = path;
+    const result = await userService.registerUser(data);
 
-  //   if (result) console.log("SUCCESSFUL");
-  // };
+    if (result) console.log("SUCCESSFUL");
+  };
 
   return (
     <div className="relative flex justify-center items-center bg-gradient-to-br from-[#1e293b] to-[#0f172a] text-white">
@@ -65,26 +65,26 @@ const Register: React.FC = () => {
         <p className="text-base text-gray-400 mb-6">
           Create your account and join us.
         </p>
-        {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div className="space-y-4">
               <TextField
                 variant="username"
                 title="Full Name*"
                 className="w-full"
-                // register={register("name")}
+                register={register("name")}
               />
               <TextField
                 variant="email"
                 title="Email*"
                 className="w-full"
-                // register={register("email")}
+                register={register("email")}
               />
               <PasswordField
                 variant="password"
                 title="Password*"
                 className="w-full"
-                // register={register("password")}
+                register={register("password")}
               />
               <Checkbox text="I agree to" link="Terms & Conditions" />
             </div>
@@ -102,7 +102,7 @@ const Register: React.FC = () => {
           <button className="w-full mt-6 text-lg font-semibold bg-blue-600 hover:bg-blue-700 text-white rounded-md py-2 transition duration-200 transform hover:scale-105">
             Register
           </button>
-        {/* </form> */}
+        </form>
 
         <p className="mt-4 text-gray-400 text-center text-sm">
           Already have an account?{" "}
