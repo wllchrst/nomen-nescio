4c2251db92209d2cb8fc85ff3429f2fc6c9bbd70
Marvel Collin <marvelcollin7@gmail.com>
2024-11-24 20:30:26 +0700

    feat: fill up home page

--- src/nomennescio_frontend/src/pages/Home.tsx
+++ src/nomennescio_frontend/src/pages/Home.tsx
@@ -61,10 +61,19 @@ const Home = () => {
             <div className="z-50">
                 <Navbar />
             </div>
-            <div className="w-screen z-30 min-h-screen flex flex-col items-center mt-20 text-white relative">
+            <div className="w-screen z-40 min-h-screen flex flex-col items-center mt-20 text-white relative">
+                <h1 className="text-9xl z-40 font-extrabold mb-2 tracking-wide relative group transition-shadow duration-300 hover:shadow-lg">
+                    Nomen Nescio
+                </h1>
+                <h2 className="text-2xl mb-6 z-40 italic relative group">
+                    /ˈnoʊ.mɛn ˈnɛs.ki.oʊ/
+                    <span className="absolute z-40 bottom-full mb-2 hidden  group-hover:block bg-gray-800 text-white text-sm p-2 rounded transition-opacity duration-300 opacity-0 group-hover:opacity-100 animate-fade-in">
+                        Pronunciation: No-men Nes-kee-o
+                    </span>
+                </h2>
                 <div className="w-screen m-4 z-40 text-center font-bold animate-fade-in">
-                    <p className='text-9xl mb-4'>From Hand to Hand</p>
-                    <p className='text-7xl mb-8'>Secured by Your&nbsp;
+                    <p className='text-4xl mb-2'>From Hand to Hand</p>
+                    <p className='text-4xl mb-8'>Secured by Your&nbsp;
                         <TypingEffect
                             text={["Mark.", "Signature."]}
                             speed={100}
