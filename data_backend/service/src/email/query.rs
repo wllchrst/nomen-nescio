7ec07fb4a8a96d7a9118995460284df474b03322
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-25 14:55:21 +0700

    feat: query for getting user email

--- data_backend/service/src/email/query.rs
+++ data_backend/service/src/email/query.rs
@@ -7,7 +7,7 @@ use sea_orm::*;
 struct EmailQuery {}
 
 impl EmailQuery {
-    async fn get_email_by_id(
+    pub async fn get_email_by_id(
         db: &DbConn,
         id: i32,
     ) -> Result<(email::Model, Vec<receiver::Model>, Vec<email_file::Model>), DbErr> {
@@ -18,5 +18,22 @@ impl EmailQuery {
         let files: Vec<email_file::Model> = email.find_related(EmailFile).all(db).await?;
 
         return Ok((email, receivers, files));
+    }
+
+    pub async fn get_user_email(db: &DbConn, user_id: i32) -> Result<Vec<email::Model>, DbErr> {
+        // Join `receiver` with `email` to filter by user_id
+        let emails = email::Entity::find()
+            .join(
+                JoinType::InnerJoin,
+                receiver::Entity::belongs_to(email::Entity)
+                    .from(receiver::Column::EmailId)
+                    .to(email::Column::Id)
+                    .into(),
+            )
+            .filter(receiver::Column::UserId.eq(user_id))
+            .all(db)
+            .await?;
+
+        Ok(emails)
     }
 }
