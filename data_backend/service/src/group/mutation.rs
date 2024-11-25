1b9c0f300d806f29189afa1eef23dc16cb0ce9c8
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-25 12:47:19 +0700

    feat: change group mutation

--- data_backend/service/src/group/mutation.rs
+++ data_backend/service/src/group/mutation.rs
@@ -1,5 +1,10 @@
 use crate::user::UserQuery;
-use ::entity::{group, group::Entity as Group, group_member, user};
+use ::entity::{
+    group::{self, Entity as Group},
+    group_member,
+    prelude::GroupMember,
+    user,
+};
 use sea_orm::*;
 
 use super::GroupQuery;
@@ -7,7 +12,7 @@ use super::GroupQuery;
 pub struct GroupMutation {}
 
 impl GroupMutation {
-    async fn create_group(
+    pub async fn create_group(
         db: &DbConn,
         group_data: group::Model,
     ) -> Result<group::ActiveModel, DbErr> {
@@ -20,47 +25,58 @@ impl GroupMutation {
         .await
     }
 
-    async fn create_group_member(
+    pub async fn add_group_members(
         db: &DbConn,
-        group_member_data: group_member::Model,
-    ) -> Result<group_member::ActiveModel, DbErr> {
-        let user: Option<user::Model> =
-            UserQuery::get_user_by_id(db, group_member_data.user_id).await?;
+        group_member_data_list: Vec<group_member::Model>, // Accept a vector of group member data
+    ) -> Result<InsertResult<group_member::ActiveModel>, DbErr> {
+        let mut active_models = Vec::new();
 
-        let group: Option<group::Model> =
-            GroupQuery::get_group_by_id(db, group_member_data.group_id).await?;
+        // Iterate over the vector of group_member::Model and process each item
+        for group_member_data in group_member_data_list {
+            let user: Option<user::Model> =
+                UserQuery::get_user_by_id(db, group_member_data.user_id).await?;
 
-        match user {
-            Some(user) => {
-                println!("User found: {:?}", user);
+            let group: Option<group::Model> =
+                GroupQuery::get_group_by_id(db, group_member_data.group_id).await?;
+
+            match user {
+                Some(user) => {
+                    println!("User found: {:?}", user);
+                }
+                None => {
+                    println!("No user found.");
+                    return Err(DbErr::Custom("User was not found".to_owned()));
+                }
             }
-            None => {
-                println!("No user found.");
-                return Err(DbErr::Custom("User was not found".to_owned()));
+
+            match group {
+                Some(group) => {
+                    println!("Group found: {:?}", group);
+                }
+                None => {
+                    println!("No group found.");
+                    return Err(DbErr::Custom("Group not found.".to_owned()));
+                }
             }
+
+            // Create ActiveModel for each group member data
+            let active_model = group_member::ActiveModel {
+                user_id: Set(group_member_data.user_id.to_owned()),
+                group_id: Set(group_member_data.group_id.to_owned()),
+                role: Set(group_member_data.role.to_owned()),
+                ..Default::default()
+            };
+
+            // Add the active model to the list
+            active_models.push(active_model);
         }
 
-        match group {
-            Some(group) => {
-                println!("Group found: {:?}", group);
-            }
-            None => {
-                println!("No group found.");
-                return Err(DbErr::Custom("Group not found.".to_owned()));
-            }
-        }
+        let results = GroupMember::insert_many(active_models).exec(db).await?;
 
-        group_member::ActiveModel {
-            user_id: Set(group_member_data.user_id.to_owned()),
-            group_id: Set(group_member_data.group_id.to_owned()),
-            role: Set(group_member_data.role.to_owned()),
-            ..Default::default()
-        }
-        .save(db)
-        .await
+        Ok(results)
     }
 
-    async fn update_group(
+    pub async fn update_group(
         db: &DbConn,
         id: i32,
         group_data: group::Model,
